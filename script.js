117ed71de7ef85522c2d6c62010969b3f3295d04
andreluizlimads <andreluizlimads@gmail.com>
2023-09-04 19:46:20 -0300

    primeiro trabalho de javascript

---
+++ script.js
@@ -0,0 +1,175 @@
+ //1
+ // let dia = true
+ // let estaClaro = "Claro"
+ // let estaEscuro = "De noite"
+
+// if(dia == true){
+//   console.log(estaClaro)
+// } else{
+// console.log(estaEscuro)
+// }
+
+
+//2
+//for(let par =0; par <=100; par+=2) {
+//    console.log(par)
+// }
+
+
+//3
+// function msgConsole() {
+//   console.log("JS não é fácil");
+// }
+//  msgConsole();
+
+//4
+// function novaMsg(nome) {
+//    console.log(`Eu sou o ` + nome);
+// }
+// novaMsg(`André`)
+
+//5
+// function informacoes(nome, idade, estilo) {
+//    console.log(`Meu nome é ` + nome);
+//    console.log(`Minha idade é ` + idade);
+//    console.log(`Meu estilo musical é ` + estilo);
+// }
+// informacoes(`André`, `27 anos`, `eclético`)
+
+
+//6
+// function filmeMusica(filme, musica) {
+//    console.log(`Recomendo o filme ` + filme);
+//    console.log(`Gosto muito da música ` + musica)
+// }
+// filmeMusica(`Entergalactic`, `Sunflower do Post Malone`)
+
+
+//7
+// function triple(numero) {
+//    return numero*3
+// }
+//console.log(triple(13))
+
+
+//8
+// function comida(lanche){
+//    if(lanche == 'cozinhar'){
+//        console.log('Você sabe cozinhar');
+//    } else if(lanche == 'pizza'){
+//        console.log('Você sabe fazer pizza');
+//    } else if(lanche == 'hamburguer'){
+//        console.log('Você sabe fazer hamburguer');
+//    } else{
+//        console.log('Você não sabe cozinhar nada');
+//    }
+//}
+
+//let resultado = comida('cozinhar');
+//let lanche = ['cozinhar', 'pizza', 'hamburguer'];
+
+
+//9
+//let harryPotter = ["Pedra Filosofal","Câmara Secreta","Prisioneiro de Azakaban","Cálice de fogo","Ordem da Fênix","Enigma do Princípe","Relíquias da Morte"]
+//console.log(harryPotter)
+
+
+//10
+//let pastel = ["carne","queijo","frango"]
+//pastel.unshift("calabresa");
+//console.log(pastel)
+
+
+//11
+//let musica = ["rock","R&B","sertanejo"]
+//musica.pop();
+//console.log(musica)
+
+
+//12
+//let jogo = ["FIFA21","FIFA22","FIFA23"]
+//jogo.push("FIFA24");
+//console.log(jogo)
+
+
+//13
+//let serie = ["Friends","The Mandalorian","Ozarks","Pink Blinders"]
+//serie.shift();
+//console.log(serie)
+
+
+//14
+//let numbers = [7,5,6,3,8,9,2,1,4]
+//console.log(numbers.sort())
+
+
+//15
+//let euMesmo = {
+//    Nome: "André",
+//    Idade: 27,
+//    UF: "RJ",
+//};
+
+
+//16
+//let euMesmo = {
+//    Nome: "André",
+//    Idade: 27,
+//    UF: "RJ",
+//};
+//euMesmo.Nacionalidade = "Brasileiro"
+
+
+//17
+//let euMesmo = {
+//    Nome: "André",
+//    Idade: 27,
+//    UF: "RJ",
+//};
+//delete euMesmo.UF
+
+
+//18
+//let euMesmo = {
+//    Nome: "André",
+//    Idade: 27,
+//    UF: "RJ",
+//};
+//delete euMesmo.UF
+//console.log(euMesmo)
+
+
+//19
+//let cadastro = [
+//    {
+//        Nome: "André",
+//        Idade: 27,
+//        Telefone: 2140028922,
+//        Amigos: "Anna, Ronaldo, Thayna, Yuri, Felipe"
+//    },
+//    {
+//        Nome: "Luiz",
+ //       Idade: 26,
+//        Telefone: 2140028922,
+//        Amigos: "Anna, Ronaldo, Thayna, Yuri, Felipe"
+//    },
+//    {
+//        Nome: "Lima",
+//        Idade: 25,
+//        Telefone: 2140028922,
+//        Amigos: "Anna, Ronaldo, Thayna, Yuri, Felipe"
+//    },
+//    {
+//        Nome: "Souza",
+//        Idade: 24,
+//        Telefone: 2140028922,
+//        Amigos: "Anna, Ronaldo, Thayna, Yuri, Felipe"
+//    },
+//    {
+//        Nome: "André Luiz",
+//        Idade: 23,
+//        Telefone: 2140028922,
+//        Amigos: "Anna, Ronaldo, Thayna, Yuri, Felipe"
+//    }
+//]
+//console.log(cadastro[0])
